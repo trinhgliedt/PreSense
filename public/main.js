83052bdce90c158ef136cf306485763e1c618245
Trinh Gliedt <chuot2008@gmail.com>
2020-12-02 12:12:51 -0800

    Added logo and formatted header bar

--- public/main.js
+++ public/main.js
@@ -4,27 +4,30 @@ var now = new Date(),
 $('#dateOfSymptoms').prop('max', maxDate);
 
 $('#aboutLink').click(function(){
-    $(this).parent().find('.narbarLink').css('border', '1px solid gray');
-    $(this).parent().find('.narbarLink').css('background-image', 'linear-gradient(to bottom right, rgb(170, 170, 170), rgba(236, 236, 232, 0.781))');
+    $(this).parent().find('.narbarLink').css('border', '1px solid rgba(179, 176, 176, 0.308)');
+    $(this).parent().find('.narbarLink').css('background-image', 'linear-gradient(rgb(170, 170, 170), rgba(221, 221, 216, 0.781), rgb(170, 170, 170))');
     $(this).css('background-image', 'linear-gradient(to bottom right, rgb(245, 245, 245), rgba(255, 255, 255, 0.993))');
+    $(this).css('border', '1px solid gray');
     $(this).css('border-bottom', '1px solid  rgb(255, 255, 255)');
     $('#about').show();
     $('#tool').hide();
     $('#tutorial').hide();
   });
 $('#toolLink').click(function(){
-  $(this).parent().find('.narbarLink').css('border', '1px solid gray');
-  $(this).parent().find('.narbarLink').css('background-image', 'linear-gradient(to bottom right, rgb(170, 170, 170), rgba(236, 236, 232, 0.781))');
+  $(this).parent().find('.narbarLink').css('border', '1px solid rgba(179, 176, 176, 0.308)');
+  $(this).parent().find('.narbarLink').css('background-image', 'linear-gradient(rgb(170, 170, 170), rgba(221, 221, 216, 0.781), rgb(170, 170, 170))');
   $(this).css('background-image', 'linear-gradient(to bottom right, rgb(245, 245, 245), rgba(255, 255, 255, 0.993))');
+  $(this).css('border', '1px solid gray');
   $(this).css('border-bottom', '1px solid  rgb(255, 255, 255)');
   $('#about').hide();
   $('#tool').show();
   $('#tutorial').hide();
 });
 $('#tutorialLink').click(function(){
-  $(this).parent().find('.narbarLink').css('border', '1px solid gray');
-  $(this).parent().find('.narbarLink').css('background-image', 'linear-gradient(to bottom right, rgb(170, 170, 170), rgba(236, 236, 232, 0.781))');
+  $(this).parent().find('.narbarLink').css('border', '1px solid rgba(179, 176, 176, 0.308)');
+  $(this).parent().find('.narbarLink').css('background-image', 'linear-gradient(rgb(170, 170, 170), rgba(221, 221, 216, 0.781), rgb(170, 170, 170))');
   $(this).css('background-image', 'linear-gradient(to bottom right, rgb(245, 245, 245), rgba(255, 255, 255, 0.993))');
+  $(this).css('border', '1px solid gray');
   $(this).css('border-bottom', '1px solid  rgb(255, 255, 255)');
   $('#about').hide();
   $('#tool').hide();
