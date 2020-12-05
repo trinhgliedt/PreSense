ff3a99024a974920797c02279d21ba0fe294855d
Trinh Gliedt <chuot2008@gmail.com>
2020-12-05 00:12:54 -0800

    form submit in progress

---
+++ public/js/formSubmit.js
@@ -0,0 +1,74 @@
+
+const submitButton = document.getElementById("submitButton");
+// const db = firebase.database();
+
+// submitButton.addEventListener('click', (e) =>{
+$("form").submit(function(e){
+        console.log('Submit button is clicked');
+        e.preventDefault();
+        // var ele = document.getElementById("FORM_ID");
+        // $('input[name="fname"]').val().checkValidity();
+        // $('input[name="fname"]').val().reportValidity();
+    console.log('Submit button is clicked');
+    e.preventDefault();
+    db.ref("formData/").set({
+        _1_provider_fname : $('input[name="fname"]').val(),
+        _1_provider_lname : $('input[name="lname"]').val(),
+        _1_provider_email : $('input[name="email"]').val(),
+        _1_provider_phone : $('input[name="phone"]').val(),
+        _1_provider_organization : $('input[name="organization"]').val(),
+
+        _2_goal_testGoal : $('input[name="testGoal"]:checked').val(),
+        _2_goal_testType : $("#testType option:selected").val(),
+        _2_goal_pcrResult : $('input[name="pcrResult"]').val(),
+        _2_goal_iggResult : $('input[name="iggResult"]').val(),
+        _2_goal_igmResult : $('input[name="igmResult"]').val(),
+
+        _3_clinical_dateOfSymptoms : $('input[name="dateOfSymptoms"]').val(),
+        _3_clinical_symptomAgeInWeek : $('input[name="symptomAgeInWeek"]').val(),
+        _3_clinical_patientAge : $('input[name="patientAge"]').val(),
+        _3_clinical_patientZipcode : $('input[name="zipcode"]').val(),
+        _3_clinical_patientFips : $('input[name="fips"]').val(),
+        _3_clinical_symptomCough : $("#symptomCough").prop("checked"),
+        _3_clinical_symptomFever : $("#symptomFever").prop("checked"),
+        _3_clinical_symptomGI : $("#symptomGI").prop("checked"),
+        _3_clinical_symptomFatigue : $("#symptomFatigue").prop("checked"),
+        _3_clinical_symptomTaste : $("#symptomTaste").prop("checked"),
+        _3_clinical_symptomNone : $("#symptomNone").prop("checked"),
+
+        _3_goal_exposureProlonged : $('#exposureProlonged').prop("checked"),
+        _3_goal_exposureBrief : $('#exposureBrief').prop("checked"),
+        _3_goal_exposurePossible : $('#exposurePossible').prop("checked"),
+
+        _4_neu_lym_neutrophil : $('input[name="neutrophil"]').val(),
+        _4_neu_lym_lymphocyte : $('input[name="lymphocyte"]').val(),
+        _4_neu_lym_nLratio : $('input[name="nLratio"]').val(),
+        _4_neu_lym_score : $('input[name="score"]').val(),
+
+        _5_radiography : $("#radiography option:selected").val(),
+
+        _6_result_prevalence : $('input[name="prevalence"]').val(),
+        _6_result_prevalenceScore : $('input[name="prevalenceScore"]').val(),
+        _6_result_totalScore : $('input[name="totalScore"]').val(),
+        _6_result_preTestProbability : $('input[name="preTestProbability"]').val(),
+        _6_result_ppv : $('input[name="ppv"]').val(),
+        _6_result_npv : $('input[name="npv"]').val(),
+        _6_result_testResult : $('input[name="testResult"]').val(),
+        _6_result_consideration: document.getElementById("consideration").innerHTML
+        
+    })
+    alert("Information submitted successfully");
+    $('#toolForm').trigger("reset");
+    $(".goalTestType").hide();
+    $(".goalPCR").hide();
+    $(".goalIgg").hide();
+    $(".goalIgm").hide();
+    $(".hiddenField").hide();
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+})
+
+
+
+
+
+// https://www.youtube.com/watch?v=mBGf_HF3kKw&ab_channel=TechWAR
---
+++ public/js/test.js
@@ -0,0 +1,91 @@
+        _1_provider_fname = $('input[name="fname"]').val();
+        _1_provider_lname = $('input[name="lname"]').val();
+        _1_provider_email = $('input[name="email"]').val();
+        _1_provider_phone = $('input[name="phone"]').val();
+        _1_provider_organization = $('input[name="organization"]').val();
+
+        _2_goal_testGoal = $('input[name="testGoal"]:checked').val();
+        _2_goal_testType = $('input[name="testType"]').val();
+        _2_goal_pcrResult = $('input[name="pcrResult"]').val();
+        _2_goal_iggResult = $('input[name="iggResult"]').val();
+        _2_goal_igmResult = $('input[name="igmResult"]').val();
+
+        _3_clinical_dateOfSymptoms = $('input[name="dateOfSymptoms"]').val();
+        _3_clinical_symptomAgeInWeek = $('input[name="symptomAgeInWeek"]').val();
+        _3_clinical_patientAge = $('input[name="patientAge"]').val();
+        _3_clinical_patientZipcode = $('input[name="zipcode"]').val();
+        _3_clinical_patientFips = $('input[name="fips"]').val();
+        _3_clinical_symptomCough = $('input[name="symptomCough"]:checked');
+        _3_clinical_symptomFever = $('input[name="symptomFever"]:checked');
+        _3_clinical_symptomGI = $('input[name="symptomGI"]:checked');
+        _3_clinical_symptomFatigue = $('input[name="symptomFatigue"]:checked');
+        _3_clinical_symptomTaste = $('input[name="symptomTaste"]:checked');
+        _3_clinical_symptomNone = $('input[name="symptomNone"]:checked');
+
+        _3_goal_exposureProlonged = $('#exposureProlonged').prop("checked");
+        _3_goal_exposureBrief = $('#exposureBrief').prop("checked");
+        _3_goal_exposurePossible = $('#exposurePossible').prop("checked");
+
+        _4_neu_lym_neutrophil = $('input[name="neutrophil"]').val();
+        _4_neu_lym_lymphocyte = $('input[name="lymphocyte"]').val();
+        _4_neu_lym_nLratio = $('input[name="nLratio"]').val();
+        _4_neu_lym_score = $('input[name="score"]').val();
+
+        _5_radiography = $('input[name="radiography"]').val();
+
+        _6_result_prevalence = $('input[name="prevalence"]').val();
+        _6_result_prevalenceScore = $('input[name="prevalenceScore"]').val();
+        _6_result_totalScore = $('input[name="totalScore"]').val();
+        _6_result_preTestProbability = $('input[name="preTestProbability"]').val();
+        _6_result_ppv = $('input[name="ppv"]').val();
+        _6_result_npv = $('input[name="npv"]').val();
+        _6_result_testResult = $('input[name="testResult"]').val();
+        _6_result_consideration= document.getElementById("consideration");
+
+
+
+        // const _1_provider_fname = $('input[name="fname"]').val();
+// const _1_provider_lname = $('input[name="lname"]').val();
+// const _1_provider_email = $('input[name="email"]').val();
+// const _1_provider_phone = $('input[name="phone"]').val();
+// const _1_provider_organization = $('input[name="organization"]').val();
+
+// const _2_goal_testGoal = $('input[name="testGoal"]:checked').val();
+// const _2_goal_testType = $('input[name="testType"]').val();
+// const _2_goal_pcrResult = $('input[name="pcrResult"]').val();
+// const _2_goal_iggResult = $('input[name="iggResult"]').val();
+// const _2_goal_igmResult = $('input[name="igmResult"]').val();
+
+// const _3_clinical_dateOfSymptoms = $('input[name="dateOfSymptoms"]').val();
+// const _3_clinical_symptomAgeInWeek = $('input[name="symptomAgeInWeek"]').val();
+// const _3_clinical_patientAge = $('input[name="patientAge"]').val();
+// const _3_clinical_patientZipcode = $('input[name="zipcode"]').val();
+// const _3_clinical_patientFips = $('input[name="fips"]').val();
+// const _3_clinical_symptomCough = $('input[name="symptomCough"]:checked');
+// const _3_clinical_symptomFever = $('input[name="symptomFever"]:checked');
+// const _3_clinical_symptomGI = $('input[name="symptomGI"]:checked');
+// const _3_clinical_symptomFatigue = $('input[name="symptomFatigue"]:checked');
+// const _3_clinical_symptomTaste = $('input[name="symptomTaste"]:checked');
+// const _3_clinical_symptomNone = $('input[name="symptomNone"]:checked');
+
+// const _3_goal_exposureProlonged = $('#exposureProlonged').prop("checked");
+// const _3_goal_exposureBrief = $('#exposureBrief').prop("checked");
+// const _3_goal_exposurePossible = $('#exposurePossible').prop("checked");
+
+// const _4_neu_lym_neutrophil = $('input[name="neutrophil"]').val();
+// const _4_neu_lym_lymphocyte = $('input[name="lymphocyte"]').val();
+// const _4_neu_lym_nLratio = $('input[name="nLratio"]').val();
+// const _4_neu_lym_score = $('input[name="score"]').val();
+
+// const _5_radiography = $('input[name="radiography"]').val();
+
+// const _6_result_prevalence = $('input[name="prevalence"]').val();
+// const _6_result_prevalenceScore = $('input[name="prevalenceScore"]').val();
+// const _6_result_totalScore = $('input[name="totalScore"]').val();
+// const _6_result_preTestProbability = $('input[name="preTestProbability"]').val();
+// const _6_result_ppv = $('input[name="ppv"]').val();
+// const _6_result_npv = $('input[name="npv"]').val();
+// const _6_result_testResult = $('input[name="testResult"]').val();
+// const _6_result_consideration= document.getElementById("consideration");
+
+// _3_clinical_symptomNone : $('input[name="symptomNone"]:checked'),
